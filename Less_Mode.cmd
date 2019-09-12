a0af7882d3746b5808050b3f22c62517e4255c48
trollfist20 <43638783+trollfist20@users.noreply.github.com>
2019-09-12 18:32:07 +0700

    Fixed Message in Less Mode Activator

--- Less_Mode.cmd
+++ Less_Mode.cmd
@@ -1,7 +1,7 @@
 ::ATOWU in Less Mode
 @echo off
 echo Less-Mode=on > %temp%\ATOWU.Less-mode
-echo Successfully Turning Less Mode 
-echo You can ATOWU now...
+echo Successfully Turning Less Mode ON
+echo You can Start ATOWU now...
 pause>NUL
 exit
